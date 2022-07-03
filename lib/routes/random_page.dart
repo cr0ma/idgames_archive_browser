fde11977460c5de408148e0afd3bf0b54e534710
cr0ma <cr0ma@tracciabi.li>
2022-07-03 09:16:51 +0200

    kinda fix random_page

--- lib/routes/random_page.dart
+++ lib/routes/random_page.dart
@@ -60,189 +60,200 @@ class _RandomEntryPageState extends State<RandomEntryPage> {
         FutureBuilder<FileElement>(
             future: _genEntryRandomId(),
             builder: (context, snapshot) {
-              return Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Table(
-                        columnWidths: const <int, TableColumnWidth>{
-                          0: IntrinsicColumnWidth(),
-                          1: FixedColumnWidth(550)
-                        },
-                        border: TableBorder.all(
-                          color: Colors.transparent,
-                          width: 0,
+              if (snapshot.hasData) {
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Table(
+                          columnWidths: const <int, TableColumnWidth>{
+                            0: IntrinsicColumnWidth(),
+                            1: FixedColumnWidth(550)
+                          },
+                          border: TableBorder.all(
+                            color: Colors.transparent,
+                            width: 0,
+                          ),
+                          children: [
+                            TableRow(
+                              children: [
+                                TableCell(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Text("Title:"),
+                                  ),
+                                ),
+                                TableCell(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Text(
+                                      snapshot.data!.title,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            TableRow(
+                              children: [
+                                TableCell(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Text("Size:"),
+                                  ),
+                                ),
+                                TableCell(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Text(filesize(
+                                        snapshot.data!.size.toString())),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            TableRow(
+                              children: [
+                                TableCell(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Text("Date:"),
+                                  ),
+                                ),
+                                TableCell(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Text(
+                                        snapshot.data!.date.toIso8601String()),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            TableRow(
+                              children: [
+                                TableCell(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Text(
+                                      "Rating",
+                                    ),
+                                  ),
+                                ),
+                                TableCell(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Text(
+                                      snapshot.data!.rating.toString(),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-                        children: [
-                          TableRow(
-                            children: [
-                              TableCell(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Text("Title:"),
-                                ),
-                              ),
-                              TableCell(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Text(
-                                    snapshot.data!.title,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          TableRow(
-                            children: [
-                              TableCell(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Text("Size:"),
-                                ),
-                              ),
-                              TableCell(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Text(
-                                      filesize(snapshot.data!.size.toString())),
-                                ),
-                              ),
-                            ],
-                          ),
-                          TableRow(
-                            children: [
-                              TableCell(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Text("Date:"),
-                                ),
-                              ),
-                              TableCell(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Text(
-                                      snapshot.data!.date.toIso8601String()),
-                                ),
-                              ),
-                            ],
-                          ),
-                          TableRow(
-                            children: [
-                              TableCell(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Text(
-                                    "Rating",
-                                  ),
-                                ),
-                              ),
-                              TableCell(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Text(
-                                    snapshot.data!.rating.toString(),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        width: 400,
-                      ),
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Flexible(
-                            flex: 0,
-                            child: new Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                child: IconButton(
-                                  onPressed: () {
-                                    ShellExecute(
-                                        NULL,
-                                        TEXT('open'),
-                                        TEXT(snapshot.data!.url),
-                                        nullptr,
-                                        nullptr,
-                                        SW_SHOWNORMAL);
-                                  },
-                                  icon: Text(
-                                    "View on /idgames",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight
-                                          .bold, /* color: Colors.green */
+                        SizedBox(
+                          width: 400,
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Flexible(
+                              flex: 0,
+                              child: new Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  child: IconButton(
+                                    onPressed: () {
+                                      ShellExecute(
+                                          NULL,
+                                          TEXT('open'),
+                                          TEXT(snapshot.data!.url),
+                                          nullptr,
+                                          nullptr,
+                                          SW_SHOWNORMAL);
+                                    },
+                                    icon: Text(
+                                      "View on /idgames",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight
+                                            .bold, /* color: Colors.green */
+                                      ),
+                                      textScaleFactor: 2,
                                     ),
-                                    textScaleFactor: 2,
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                          Flexible(
-                            flex: 0,
-                            child: new Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                child: IconButton(
-                                  onPressed: () {
-                                    ShellExecute(
-                                        0,
-                                        TEXT('open'),
-                                        TEXT('notepad.exe'),
-                                        TEXT(
-                                            "C:/Giochi/DOOM/mirror/pc/games/idgames/${snapshot.data!.dir}/${snapshot.data!.filename.replaceAll(RegExp('zip'), 'txt')}"),
-                                        nullptr,
-                                        SW_SHOW);
-                                  },
-                                  icon: Text(
-                                    "Read textfile",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
+                            Flexible(
+                              flex: 0,
+                              child: new Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  child: IconButton(
+                                    onPressed: () {
+                                      ShellExecute(
+                                          0,
+                                          TEXT('open'),
+                                          TEXT('notepad.exe'),
+                                          TEXT(
+                                              "C:/Giochi/DOOM/mirror/pc/games/idgames/${snapshot.data!.dir}/${snapshot.data!.filename.replaceAll(RegExp('zip'), 'txt')}"),
+                                          nullptr,
+                                          SW_SHOW);
+                                    },
+                                    icon: Text(
+                                      "Read textfile",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      textScaleFactor: 2,
                                     ),
-                                    textScaleFactor: 2,
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                          Flexible(
-                            flex: 0,
-                            child: new Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                child: IconButton(
-                                  onPressed: () {
-                                    ShellExecute(
-                                        0,
-                                        TEXT('open'),
-                                        // TODO: Make the user pick the sourceport
-                                        TEXT('C:/Giochi/DOOM/gzdoom.exe'),
-                                        TEXT(
-                                            "C:/Giochi/DOOM/mirror/pc/games/idgames/${snapshot.data!.dir}/${snapshot.data!.filename}"),
-                                        nullptr,
-                                        SW_SHOW);
-                                  },
-                                  icon: Text(
-                                    "Play",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
+                            Flexible(
+                              flex: 0,
+                              child: new Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  child: IconButton(
+                                    onPressed: () {
+                                      ShellExecute(
+                                          0,
+                                          TEXT('open'),
+                                          // TODO: Make the user pick the sourceport
+                                          TEXT('C:/Giochi/DOOM/gzdoom.exe'),
+                                          TEXT(
+                                              "C:/Giochi/DOOM/mirror/pc/games/idgames/${snapshot.data!.dir}/${snapshot.data!.filename}"),
+                                          nullptr,
+                                          SW_SHOW);
+                                    },
+                                    icon: Text(
+                                      "Play",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      textScaleFactor: 2,
                                     ),
-                                    textScaleFactor: 2,
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
-                      )
-                    ],
+                          ],
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              );
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                    child: Expanded(
+                        child: Text(
+                  "No data from API, try again",
+                  textScaleFactor: 2,
+                )));
+              }
+              return Expanded(
+                  child: Center(child: mat.CircularProgressIndicator()));
             }),
       ],
     );
